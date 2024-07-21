b89ece1ef1ee00005c4d55468f833144e585dbdc
Nemo <altair.infamous@yahoo.com>
2024-07-21 07:26:14 +0330

    "Refactored Rust code for neural network library, updated Python interface and test script."

--- src/linear.rs
+++ src/linear.rs
@@ -1,28 +1,18 @@
-use crate::{layer::Layers, random_bias, random_weight, DnArrayAs, Ndarray};
-use ndarray::{Array1, ArrayBase, ArrayD, Dim, IxDyn, IxDynImpl, OwnedRepr};
-
-use numpy::{dot_bound, npyffi::npy_float, IntoPyArray, PyArray, PyArrayDyn};
+use crate::{layer::Layers, random_array};
+use ndarray::{ArrayBase, ArrayD, Dim, IxDyn, IxDynImpl, OwnedRepr};
+use numpy::{IntoPyArray, PyArrayDyn};
 use pyo3::{
     prelude::*,
-    types::{IntoPyDict, PyDict},
+    types::{IntoPyDict, PyDict, PyTuple},
 };
-/// Type alias for a 1-dimensional ndarray with owned data and dynamic dimensions.
 
-///# A Python class representing a linear layer in a neural network.
-///
-///## Attributes:
-///*     module (str): The name of the module, which is "layer".
-///*     name (str): The name of the class, which is "Linear".
-///*     unsendable (bool): Indicates that the class is unsendable.
-///*     extends (Layers): Indicates that the class extends the Layers class.
-///*     subclass (bool): Indicates that the class can be subclassed.
-///*     sequence (bool): Indicates that the class behaves like a sequence.
-///*     dict (bool): Indicates that the class has a dictionary attribute.
-///*     get_all (bool): Indicates that all attributes are gettable.
-///*     set_all (bool): Indicates that all attributes are settable.
-// #[derive(FromPyObject)]
+pub type Ndarray = ArrayBase<OwnedRepr<f64>, Dim<IxDynImpl>>;
+pub type NDArray2 = ArrayBase<OwnedRepr<Vec<Vec<f64>>>, Dim<[usize; 2]>>;
+pub type Object = Py<PyArrayDyn<f64>>;
+
+#[derive(FromPyObject)]
 #[pyclass(
-    module = "layer",
+    module = "nn",
     name = "Linear",
     unsendable,
     extends= Layers,
@@ -34,73 +24,62 @@ use pyo3::{
 )]
 
 pub struct Linear {
-    /// The weights of the linear layer.
-    pub weight: PyObject,
-    /// The bias of the linear layer.
+    pub weights: PyObject,
     pub bias: PyObject,
-    /// Indicates whether the layer uses a bias term.
     pub is_bias: bool,
-    /// Indicates whether the layer is trainable.
-    pub trainable: bool,
-    /// The shape of the linear layer as a tuple (in_features, out_features).
-    shape: (usize, usize),
+    shape: (u16, u16),
 }
 
 #[pymethods]
 impl Linear {
-    ///## Creates a new instance of the Linear class.
-    ///
-    ///### Args:
-    ///*     py (Python): The Python GIL token.
-    ///*     in_features (u16): The number of input features.
-    ///*     out_features (u16): The number of output features.
-    ///*     is_bias (Option<bool>): Whether the layer uses a bias term.
-    ///*     trainable (Option<bool>): Whether the layer is trainable.
-    ///*     args (Bound<'_, PyAny>): Positional arguments.
-    ///*     kwargs (Option<Bound<'_, PyAny>>): Keyword arguments.
-    ///
-    /// Returns:
-    ///     PyResult<(Self, Layers)>: A new instance of the Linear class and its base Layers class.
     #[new]
-    #[pyo3(signature = (in_features , out_features, is_bias = true , trainable = true,  *args , **kwargs  ))]
-    #[allow(unused_variables)]
+    #[pyo3(signature = (in_features , out_features, is_bias = true , *args , **kwargs  ))]
     pub fn __new__<'py>(
         py: Python,
-        in_features: usize,
-        out_features: usize,
+        in_features: u16,
+        out_features: u16,
         is_bias: Option<bool>,
-        trainable: Option<bool>,
         args: &Bound<'_, PyAny>,
         kwargs: Option<&Bound<'_, PyAny>>,
     ) -> PyResult<(Self, Layers)> {
+        // let super_ = slf.as_super(); // Get &PyRef<BaseClass>
         let is_bias = match is_bias {
             Some(is_bias) => is_bias,
             None => false,
         };
 
-        let random_weight: Ndarray<IxDynImpl> =
-            random_weight(in_features.into(), out_features.into()).expect("faild to make random weight");
-        let random_bias: Ndarray<[usize; 1]> = if is_bias {
-            let r_bias: ArrayBase<OwnedRepr<f32>, Dim<[usize; 1]>> =
-                random_bias(out_features.into()).expect("faild to make random bias");
-            r_bias
+        let random_weight: Ndarray = random_array(in_features.into(), out_features.into());
+        let random_bias: Ndarray = if is_bias {
+            random_array(1, out_features.into())
         } else {
-            let zero_bias: ArrayBase<OwnedRepr<f32>, Dim<[usize; 1]>> = Array1::zeros(out_features);
-            zero_bias
+            ArrayD::zeros(IxDyn(&[1, out_features.into()]))
         };
 
         let result = (
             Self {
-                weight: random_weight.into_pyarray_bound(py).to_owned().into(),
+                weights: random_weight.into_pyarray_bound(py).to_owned().into(),
                 bias: random_bias.into_pyarray_bound(py).to_owned().into(),
                 is_bias: is_bias,
-                trainable: trainable.unwrap_or(true),
                 shape: (in_features, out_features),
             },
             Layers,
         );
         Ok(result)
     }
+    // Python::with_gil(|py| {
+    //     let random_weight: Ndarray = random_array(in_features.into(), out_features.into());
+    //     let random_bias: Ndarray = if is_bias {
+    //         random_array(1, out_features.into())
+    //     } else {
+    //         ArrayD::zeros(IxDyn(&[1, out_features.into()]))
+    //     };
+    //     Ok(Self {
+    //         weights: random_weight.into_pyarray_bound(py).to_owned().into(),
+    //         bias: random_bias.into_pyarray_bound(py).to_owned().into(),
+    //         is_bias,
+    //         shape: (in_features, out_features),
+    //     })
+    // })
 
     #[pyo3(text_signature = "($cls )")]
     fn parameters<'py>(slf: &Bound<Self>, _py: Python<'py>) -> Py<PyDict> {
@@ -116,20 +95,31 @@ impl Linear {
         return dict.unbind();
     }
 
-    fn __call__(slf: &Bound<Self>, py: Python<'_>, value: &Bound<PyAny>) -> PyResult<PyObject> {
-        //cast `value` in to ndarray
-        let value: &PyArrayDyn<npy_float> = value.extract()?;
-        let value: DnArrayAs<npy_float> = value.to_owned_array();
-        let weight: &PyArrayDyn<npy_float> = slf.borrow().weight.extract(py)?;
-        let weight: DnArrayAs<npy_float> = weight.to_owned_array().t().to_owned();
+    fn __call__(&self, py: Python<'_>, value: &Bound<PyAny>) -> PyResult<PyObject> {
+        // Convert value to PyArrayDyn
+        let value: &PyArrayDyn<f64> = value.extract()?;
 
-        let result: Bound<PyArrayDyn<npy_float>> = dot_bound(
-            &value.into_pyarray_bound(py),
-            &weight.into_pyarray_bound(py),
-        )
-        .unwrap();
+        // Get NumPy module
+        let np = PyModule::import_bound(py, "numpy")?;
+        // Transpose value
+        // let transpose = np.getattr("transpose")?.call1((self.weights.clone(),))?;
+        // Perform dot product: self.weights \dot value
+        let weight = self.weights.bind(py);
+        let bias = self.bias.bind(py);
 
-        let result = result.add(slf.borrow().bias.to_owned())?;
+        let dot_result = np
+            .getattr("dot")
+            .expect("Invalid import dot")
+            .call1((value, weight))
+            .expect("dot prod error");
+
+        // Add bias: (self.weights \dot value) + self.bias
+        let result = np
+            .getattr("add")
+            .expect("error import add")
+            .call1((dot_result, bias))
+            .expect("summation error");
+
         Ok(result.to_object(py))
     }
 
@@ -141,11 +131,10 @@ impl Linear {
         };
         let class_name = slf.get_type().qualname().unwrap();
         format!(
-            "{}(in_features = {}, out_features = {}, bias={}, params={}) ",
+            "{}(in = {},out = {}, params={}) ",
             class_name,
             slf.borrow().shape.0,
             slf.borrow().shape.1,
-            slf.borrow().is_bias,
             slf.borrow().shape.0 * slf.borrow().shape.1 + bias_shape
         )
     }
@@ -153,7 +142,12 @@ impl Linear {
     fn __repr__(slf: &Bound<Self>) -> PyResult<String> {
         // This is the equivalent of `self.__class__.__name__` in Python.
         let class_name = slf.get_type().qualname()?;
-        Ok(format!("{}", class_name))
+        Ok(format!(
+            "{}(weights : {} , bias : {})",
+            class_name,
+            slf.borrow().weights,
+            slf.borrow().bias
+        ))
     }
 
     #[getter]
@@ -169,7 +163,7 @@ impl Linear {
         Python::with_gil(|py| {
             // let list = PyList::new_bound(py, slf.borrow().weights.clone().to_object(py));
             let locals = [
-                ("weighs", slf.borrow().weight.clone()),
+                ("weighs", slf.borrow().weights.clone()),
                 ("self", class_name.to_object(py)),
                 ("bias", slf.borrow().bias.clone()),
             ]
